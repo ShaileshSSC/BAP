15f5e350e559c0a2969a7d2203fb692b83404104
ShaileshSSC <31829252+ShaileshSSC@users.noreply.github.com>
2018-03-05 09:13:34 +0100

    Delete sendmail.php

--- sendmail.php
+++
@@ -1,9 +0,0 @@
-<?php
-$to = '24571@ma-web.nl';
-$subject = $_POST['subject'];
-$message = $_POST['message'];
-$headers = 'From: ' . $_POST['from'];
-
-mail($to, $subject, $message, $headers);
-
-echo 'Uw mail is verzonden!';
