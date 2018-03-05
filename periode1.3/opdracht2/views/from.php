da37468e025021b8b220d8c5c203b3ab26cfad4a
ShaileshSSC <31829252+ShaileshSSC@users.noreply.github.com>
2018-03-05 09:44:14 +0100

    Add files via upload

---
+++ periode1.3/opdracht2/views/from.php
@@ -0,0 +1,16 @@
+<h1>Contactformulier</h1>
+<h3>Shailesh chotoe MG1D</h3>
+<hr>
+<pre>
+  <form action="views/response.php" method="post">
+
+    naam: 	 <input type="text" name="name" autofocus>
+
+    email: 	  <input type="email" name="email">
+
+    bericht: 	<input type="text" name="message">
+
+    			    <input type="submit" name='sendContact'>
+  </form>
+</pre>
+<hr>
---
+++ periode1.3/opdracht2/views/response.php
@@ -0,0 +1,9 @@
+<?php
+$naam = $_POST['name'];
+$mail = $_POST['email'];
+$message = $_POST['message'];
+echo '<h2>Hallo wereld! </h2>';
+echo 'Mijn naam is '.$_POST['name'].'<br/ >';
+echo 'Mijn e-mail is '.$_POST['email'].'<br/ >';
+echo 'Ik zeg: '.$_POST['message'];
+?>
